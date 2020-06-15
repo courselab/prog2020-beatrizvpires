882fbb75d8db0cd5831d76e1227df987d88ccb78
beatrizvpires <biavpiires@usp.br>
2020-06-15 20:36:58 +0000

    Primeira Entrega m003

--- m003.c
+++ m003.c
@@ -23,7 +23,26 @@
 
 int is_prime (int a)
 {
-  return 0;
+   int i;
+  int count = 0;
+  for(i=1; i<a ;i++)
+  {
+    if((a%i)==0)
+    {
+      count++;
+    }
+  } 
+
+  if(count == 1)
+
+  {
+    a=1;
+  }
+  else
+  {
+    a=0;
+  }
+  return a;
 }
 
 #define USAGE "m003 <num>\n"
